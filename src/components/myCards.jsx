8e2240be3c3ecb7af813cbff4a9c5e3171c600ba
Varun Dhanda <157876914+varun-dhanda@users.noreply.github.com>
2026-06-23 15:31:05 +0530

    Add navigation and state management to MyCards component

--- src/components/myCards.jsx
+++ src/components/myCards.jsx
@@ -1,9 +1,8 @@
+import { useEffect, useState } from "react";
+
 const Box = ({ myProduct }) => {
   return (
-    <div
-     className="card"
-      key={myProduct.id}
-    >
+    <div className="card" key={myProduct.id}>
       <h2>{myProduct.name}</h2>
       <div>{myProduct.category}</div>
       <div>{myProduct.price}</div>
@@ -12,14 +11,82 @@ const Box = ({ myProduct }) => {
 };
 
 const MyCards = ({ products, heading = "heading" }) => {
-  console.log(products, "products");
+  const [pageData, setPageData] = useState(products[0]);
+  console.log(setPageData, typeof setPageData, "setPageData");
 
+  useEffect(() => {
+    console.log(pageData, "pageDatasda");
+  }, [pageData]);
+
+  const handleStateChange = (data) => {
+    setPageData({ ...data });
+  };
+
+  console.log("hello")
   return (
     <>
-      <h1>{heading}</h1>
-      {products.map((value) => {
-        return <Box key={value.id} myProduct={value} />;
-      })}
+      <nav className="nav">
+        <h1>Logo</h1>
+        <div className={`navItems`}>
+          <a
+            onClick={() => {
+              handleStateChange({
+                heading: "home",
+                category: "Home category",
+                name: "Home name",
+                price: 100,
+                className: "home",
+                khusi:"khusi",
+              });
+            }}
+          >
+            home
+          </a>
+          <a
+            onClick={() => {
+              handleStateChange({
+                heading: "Services",
+                category: "Services category",
+                name: "Services name",
+                price: 200,
+                className: "services",
+              });
+            }}
+          >
+            services
+          </a>
+          <a
+            onClick={() => {
+              handleStateChange({
+                heading: "Contact",
+                category: "Contact category",
+                name: "Contact name",
+                price: 300,
+                className: "contact",
+              });
+            }}
+          >
+            contact
+          </a>
+          <a
+            onClick={() => {
+              handleStateChange({
+                heading: "About",
+                category: "About category",
+                name: "About name",
+                price: 400,
+                className: "about",
+              });
+            }}
+          >
+            about
+          </a>
+        </div>
+      </nav>
+      <div className={`container ${pageData.className}`}>
+        <h1 style={{}}>{pageData.heading || heading}</h1>
+        <Box myProduct={pageData} />
+      </div>
     </>
   );
 };
